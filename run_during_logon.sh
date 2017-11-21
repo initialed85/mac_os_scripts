a126817f3356bff68e2793bc37ca7ee23f117980
Edward Beech <initialed85@gmail.com>
2017-11-21 21:47:34 +0800

    amendments

--- run_during_logon.sh
+++ run_during_logon.sh
@@ -8,10 +8,10 @@
 # two bash scripts
 
 # fully qualified domain name of the file server
-FQDN=grayfs01.grayman.com.au
+FQDN='grayfs01.grayman.com.au'
 
 # will be suffixed with \(current username)
-SHARE_PREFIX=homedrives\$
+SHARE_PREFIX='homedrives$'
 
 # need to run scripts from here because of Python path requirements
 cd /usr/local/zetta/
@@ -32,4 +32,4 @@ python -m mac_os_scripts.enable_reduced_transparency
 python -m mac_os_scripts.disable_metadata_file_creation
 
 # map user drive; note: -s flag is share prefix (not full share), it'll be suffixed with the username as a folder
-python -m mac_os_scripts.map_user_drive -f $FQDN -s $SHARE_PREFIX
+python -m mac_os_scripts.map_user_drive -f "$FQDN"-s "$SHARE_PREFIX"
